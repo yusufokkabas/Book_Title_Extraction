0e035ea42186209886a6639968b2d56b6a27790a
yusufokkabas@windowslive.com <yusufokkabas@windowslive.com>
2023-12-15 14:21:51 +0300

    text_detection

--- main.py
+++ main.py
@@ -1,10 +1,126 @@
+import numpy as np
+import cv2
 from PIL import Image
 import pytesseract
-import cv2
-import numpy as np
 
-# Load the image from file
-image = cv2.imread('suc.jpeg')
+def decode_predictions(scores, geometry):
+    # grab the number of rows and columns from the scores volume, then
+    # initialize our set of bounding box rectangles and corresponding
+    # confidence scores
+    (numRows, numCols) = scores.shape[2:4]
+    rects = []
+    confidences = []
+
+    # loop over the number of rows
+    for y in range(0, numRows):
+        # extract the scores (probabilities), followed by the
+        # geometrical data used to derive potential bounding box
+        # coordinates that surround text
+        scoresData = scores[0, 0, y]
+        xData0 = geometry[0, 0, y]
+        xData1 = geometry[0, 1, y]
+        xData2 = geometry[0, 2, y]
+        xData3 = geometry[0, 3, y]
+        anglesData = geometry[0, 4, y]
+
+        # loop over the number of columns
+        for x in range(0, numCols):
+            # if our score does not have sufficient probability,
+            # ignore it
+            if scoresData[x] < 0.5:
+                continue
+
+            # compute the offset factor as our resulting feature
+            # maps will be 4x smaller than the input image
+            (offsetX, offsetY) = (x * 4.0, y * 4.0)
+
+            # extract the rotation angle for the prediction and
+            # then compute the sin and cosine
+            angle = anglesData[x]
+            cos = np.cos(angle)
+            sin = np.sin(angle)
+
+            # use the geometry volume to derive the width and height
+            # of the bounding box
+            h = xData0[x] + xData2[x]
+            w = xData1[x] + xData3[x]
+
+            # compute both the starting and ending (x, y)-coordinates
+            # for the text prediction bounding box
+            endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+            endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+            startX = int(endX - w)
+            startY = int(endY - h)
+
+            # add the bounding box coordinates and probability score
+            # to our respective lists
+            rects.append((startX, startY, endX, endY))
+            confidences.append(scoresData[x])
+
+    # return a tuple of the bounding boxes and associated confidences
+    return (rects, confidences)
+
+def non_max_suppression(boxes, probs=None, overlapThresh=0.3):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+
+    # if the bounding boxes are integers, convert them to floats -- this
+    # is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+
+    # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by their bottom-right y-coordinate
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+
+    # keep looping while some indexes still remain in the indexes list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the index
+        # value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of the
+        # bounding box and the smallest (x, y) coordinates for the end
+        # of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have overlap
+        # greater than the provided overlap threshold
+        idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlapThresh)[0])))
+
+    # return only the bounding boxes that were picked
+    return boxes[pick].astype("int")
+
+
+
+# Load the pre-trained EAST text detector
+net = cv2.dnn.readNet('frozen_east_text_detection.pb')
+
+# Load the image
+image = cv2.imread('savas.jpeg')
 
 # Rescale the image
 image = cv2.resize(image, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
@@ -20,14 +136,28 @@ gray = cv2.erode(gray, kernel, iterations=1)
 # Apply threshold to get image with only black and white
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# Save the preprocessed image temporarily
-cv2.imwrite('temp.png', thresh)
+# Prepare the image for text detection
+blob = cv2.dnn.blobFromImage(image, 1.0, (320, 320), (123.68, 116.78, 103.94), swapRB=True, crop=False)
+net.setInput(blob)
+(scores, geometry) = net.forward(['feature_fusion/Conv_7/Sigmoid', 'feature_fusion/concat_3'])
 
-# Open the image with PIL so pytesseract can use it
-image = Image.open('temp.png')
+# Decode the predictions from the EAST detector
+(rects, confidences) = decode_predictions(scores, geometry)
+boxes = non_max_suppression(np.array(rects), probs=confidences)
 
-# Use pytesseract to do OCR on the image
-text = pytesseract.image_to_string(image,lang='tur')
+# Loop over the bounding boxes
+for (startX, startY, endX, endY) in boxes:
+    # Extract the region of interest
+    roi = thresh[startY:endY, startX:endX]
 
-# Print the text
-print(text)
+    # Save the preprocessed ROI temporarily
+    cv2.imwrite('temp.png', roi)
+
+    # Open the ROI with PIL so pytesseract can use it
+    roi = Image.open('temp.png')
+
+    # Use pytesseract to do OCR on the ROI
+    text = pytesseract.image_to_string(roi, lang='tur')
+
+    # Print the text
+    print(text)
